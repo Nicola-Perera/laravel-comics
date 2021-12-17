3545b604ea1d73b161c767677f91ab83a9286026
Nicola-Perera <nicola.perera96@mail.com>
2021-12-17 17:51:52 +0100

    add app in layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>DC-Comics</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+
+<body>
+
+    <!-- header -->
+    @include('partials.header')
+
+    <!-- main -->
+    <main id="site_main" class="py-5">
+        @yield('content')
+    </main>
+
+    <!-- footer -->
+    @include('partials.footer')
+
+</body>
+
+</html>
