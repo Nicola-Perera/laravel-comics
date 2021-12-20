c87971ac0ce53b78ff9ca89c6b341405f2208e63
Nicola-Perera <nicola.perera96@mail.com>
2021-12-20 18:54:19 +0100

    add bottom header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -14,5 +14,51 @@
     <!-- bottom -->
         <div class="bottom_header">
 
+        <!-- logo -->
+            <div class="width70 container">
+                <div class="logo py-4">
+                    <img src="../../img/images/dc-logo.png" alt="">
+                </div>
+
+        <!-- nav menu -->
+            <div class="nav container">
+                <div class="link">
+                    <a href="">CHARCTERS</a>
+                </div>
+                <div class="link">
+                    <a href="">COMICS</a>
+                </div>
+                <div class="link">
+                    <a href="">MOVIES</a>
+                </div>
+                <div class="link">
+                    <a href="">TV</a>
+                </div>
+                <div class="link">
+                    <a href="">GAMES</a>
+                </div>
+                <div class="link">
+                    <a href="">COLLECTIBLES</a>
+                </div>
+                <div class="link">
+                    <a href="">VIDEOS</a>
+                </div>
+                <div class="link">
+                    <a href="">FANS</a>
+                </div>
+                <div class="link">
+                    <a href="">NEWS</a>
+                </div>
+                <div class="link">
+                    <a href="">SHOP</a>
+                </div>
+            </div>
+
+            <div class="search">
+               <a href="">Search</a>
+            </div>
+
+            <!-- search -->
+            </div>
         </div>
     </header>
