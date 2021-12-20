22b865960a0d0fb1f880da6d0333a3c5c1d9774c
Nicola-Perera <nicola.perera96@mail.com>
2021-12-20 18:12:41 +0100

    add top header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,14 @@
 
-    <header>Header
+    <header>
 
     <!-- top -->
-        <div class="top_header">
-            <h3>DC POWER VISA</h3>
+        <div class="top_header py-1">
+            <div class="width70">
+                <div class="container">
+                    <h3>DC POWER VISA</h3>
+                    <h3>ADDITIONAL DC SITES</h3>
+                </div>
+            </div>
         </div>
 
     <!-- bottom -->
